88f7e7b3f02f60d4db412a6481cd1d701ef1b628
Emil Nilsson <emil.nilsson@dataductus.se>
2023-04-21 17:08:09 +0200

    Add example of component inline template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: "<h1>hello from inline template</h1>",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
